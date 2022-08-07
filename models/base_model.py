ea6716b98e7537670ee1a47c7003621fca21b669
=Nii <=nii4tettey@gmail.com>
2022-08-08 00:44:49 +0200

    console & storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
 Contains the "BaseModel" class
 """
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -14,10 +15,19 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
