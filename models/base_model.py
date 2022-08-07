9dbcf858a80515e53bfe1fa4d2ab231d4867de35
=Nii <=nii4tettey@gmail.com>
2022-08-07 23:56:22 +0200

    Pycodestyle fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,8 @@ Contains the "BaseModel" class
 """
 from uuid import uuid4
 from datetime import datetime
- 
+
+
 class BaseModel:
     """ defines all common attributes/methods for other classes:"""
     def __init__(self, *args, **kwargs):
@@ -13,11 +14,11 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
- 
+
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
- 
+
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
         Includes the key/value pair __class__ representing
@@ -28,11 +29,8 @@ class BaseModel:
         rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
         return rdict
- 
+
     def _str_(self):
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
- 
- 
- 
